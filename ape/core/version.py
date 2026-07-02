16165837a14f7502d1bc439594c6815663747f89
bvnthb123 <bvnthb123@gmail.com>
2026-07-02 20:26:06 +0700

    feat: add version metadata

---
+++ ape/core/version.py
@@ -0,0 +1,9 @@
+"""
+APE - Adaptive Prediction Engine
+Version metadata.
+"""
+APP_NAME = "Adaptive Prediction Engine"
+APP_CODE = "APE"
+VERSION = "0.1.1"
+BUILD_NAME = "Core Foundation"
+AUTHOR = "Nguyen Thanh Chung + ChatGPT"

903613ec41363ead77533bee9a926b5e715dcaf8
bvnthb123 <bvnthb123@gmail.com>
2026-07-03 10:09:25 +0700

    chore: bump version to v0.5.0

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "0.4.0"
-BUILD_NAME = "Statistics & Data Audit"
+VERSION = "0.5.0"
+BUILD_NAME = "Desktop GUI"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

1b3b91bd76f6c37c835ec6c40f3d815236eb39c9
bvnthb123 <bvnthb123@gmail.com>
2026-07-03 07:48:45 +0700

    feat(database): export database layer API

--- ape/database/__init__.py
+++ ape/database/__init__.py
@@ -1,1 +1,36 @@
-"""Package initialization."""
+"""APE database package."""
+
+from ape.database.base import Base
+from ape.database.database import DATABASE, DatabaseManager
+from ape.database.models import (
+    Draw,
+    EngineScore,
+    Experiment,
+    Feature,
+    Prediction,
+    Rule,
+    RunRecord,
+)
+from ape.database.repositories import (
+    BaseRepository,
+    DrawRepository,
+    PredictionRepository,
+    RuleRepository,
+)
+
+__all__ = [
+    "Base",
+    "DATABASE",
+    "DatabaseManager",
+    "Draw",
+    "Feature",
+    "Prediction",
+    "EngineScore",
+    "Rule",
+    "Experiment",
+    "RunRecord",
+    "BaseRepository",
+    "DrawRepository",
+    "PredictionRepository",
+    "RuleRepository",
+]

0601f107234fa9d0af47d7523558fe39655a1983
bvnthb123 <bvnthb123@gmail.com>
2026-07-03 08:46:00 +0700

    chore: bump version to v0.3.0

--- ape/core/version.py
+++ ape/core/version.py
@@ -1,9 +1,7 @@
-"""
-APE - Adaptive Prediction Engine
-Version metadata.
-"""
+"""APE version metadata."""
+
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "0.2.0"
-BUILD_NAME = "Database Layer"
+VERSION = "0.3.0"
+BUILD_NAME = "Excel Importer & Data Validation"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

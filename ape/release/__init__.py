39708b157abb9707c405592393a87eb5a93937df
bvnthb123 <bvnthb123@gmail.com>
2026-07-15 20:48:02 +0700

    feat(release): initialize release utilities package

---
+++ ape/release/__init__.py
@@ -0,0 +1,5 @@
+"""Release utilities for APE desktop builds."""
+
+from ape.release.backup import BackupError, BackupManager, BackupResult
+
+__all__ = ["BackupError", "BackupManager", "BackupResult"]

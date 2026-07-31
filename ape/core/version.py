f1ba4f8cb99f75360ce683e94d7c018624a9ecf8
bvnthb123 <bvnthb123@gmail.com>
2026-07-31 22:35:45 +0700

    Bump version to 1.1.0

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "1.0.0"
-BUILD_NAME = "Portable Release & QA"
+VERSION = "1.1.0"
+BUILD_NAME = "Pattern Mining & Backtest"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

a38631b6f7f9397061b70ec00c9cc18a9e792d94
bvnthb123 <bvnthb123@gmail.com>
2026-08-01 17:03:06 +0700

    Bump version for repeat overlap learning

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "1.2.0"
-BUILD_NAME = "Structural Pattern Learning"
+VERSION = "1.3.0"
+BUILD_NAME = "Repeat Overlap Learning"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

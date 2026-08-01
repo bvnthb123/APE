b6a8ceaf81296f5507647051f08803b24f22b32e
bvnthb123 <bvnthb123@gmail.com>
2026-08-01 13:46:47 +0700

    Export structural pattern classes

--- ape/patterns/__init__.py
+++ ape/patterns/__init__.py
@@ -3,13 +3,17 @@
 from ape.patterns.mining import (
     BacktestSummary,
     CandidateSignal,
+    DrawStructure,
     PatternMiner,
     PatternRule,
+    StructureProfile,
 )
 
 __all__ = [
     "BacktestSummary",
     "CandidateSignal",
+    "DrawStructure",
     "PatternMiner",
     "PatternRule",
+    "StructureProfile",
 ]

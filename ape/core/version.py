d23e6a82a5a91d84afaac5c95771172de50b54e9
bvnthb123 <bvnthb123@gmail.com>
2026-08-10 23:23:57 +0700

    Bump version to v1.6.0 strategy audit replay

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "1.5.0"
-BUILD_NAME = "Target-Hit Strategy Optimizer"
+VERSION = "1.6.0"
+BUILD_NAME = "Strategy Audit Replay"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

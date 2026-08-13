b4977b6a1d530b846dbab2cfd33a70435fba1991
bvnthb123 <bvnthb123@gmail.com>
2026-08-13 19:56:11 +0700

    Bump version to v1.7.0 manual top 7 recalculation

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "1.6.1"
-BUILD_NAME = "Fast Strategy Audit"
+VERSION = "1.7.0"
+BUILD_NAME = "Manual Top 7 Recalculation"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

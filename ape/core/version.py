b176a510415d41867463d8d86dc98c902ff0b4c0
bvnthb123 <bvnthb123@gmail.com>
2026-08-15 18:45:39 +0700

    Update version for stable signal rollback

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "1.7.0"
-BUILD_NAME = "Manual Top 7 Recalculation"
+VERSION = "1.7.1"
+BUILD_NAME = "Stable Signal Rollback"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

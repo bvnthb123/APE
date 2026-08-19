39bb54b8446a6b6c1fc9c7e97d1b889e2fc2c3d0
bvnthb123 <bvnthb123@gmail.com>
2026-08-19 18:05:25 +0700

    Bump version for data freshness display

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "2.0.0"
-BUILD_NAME = "Target Learning Lab"
+VERSION = "2.0.1"
+BUILD_NAME = "Target Learning Data Status"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

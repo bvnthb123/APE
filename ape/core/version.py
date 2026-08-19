37516fcf6f89f9fa8279e6c5e2b6eb93a29b2039
bvnthb123 <bvnthb123@gmail.com>
2026-08-19 18:48:34 +0700

    Bump version for safe target learning preview

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "2.0.1"
-BUILD_NAME = "Target Learning Data Status"
+VERSION = "2.0.2"
+BUILD_NAME = "Safe Target Learning Preview"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

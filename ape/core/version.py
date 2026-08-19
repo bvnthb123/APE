cd4b0b9f55bebbf2b489722e4f3c249ee61c2721
bvnthb123 <bvnthb123@gmail.com>
2026-08-19 21:25:56 +0700

    Bump version for expanded target learning search

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "2.1.0"
-BUILD_NAME = "Latest Excel Data Refresh"
+VERSION = "2.1.1"
+BUILD_NAME = "Expanded Target Method Search"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

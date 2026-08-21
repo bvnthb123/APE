42110af58ff296966c8be05fe9c67ab9696551a9
bvnthb123 <bvnthb123@gmail.com>
2026-08-21 22:38:26 +0700

    Bump version for target rescue search

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "2.2.0"
-BUILD_NAME = "Full Data Replacement"
+VERSION = "2.2.1"
+BUILD_NAME = "Target Rescue Search"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

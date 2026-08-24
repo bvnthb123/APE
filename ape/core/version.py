a820a3f3322ac625f310b68587515f91d5bd24e6
bvnthb123 <bvnthb123@gmail.com>
2026-08-24 18:03:01 +0700

    Bump version for target diagnostic gate

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "2.2.1"
-BUILD_NAME = "Target Rescue Search"
+VERSION = "2.2.2"
+BUILD_NAME = "Target Diagnostic Gate"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

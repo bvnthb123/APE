80b86e8d32b62eb126e139edd9c5bb63f0106cf6
bvnthb123 <bvnthb123@gmail.com>
2026-08-24 19:38:48 +0700

    Bump version for historical holdout preview

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "2.2.2"
-BUILD_NAME = "Target Diagnostic Gate"
+VERSION = "2.2.3"
+BUILD_NAME = "Historical Holdout Preview"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

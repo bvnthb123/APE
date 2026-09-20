054099dbb1f170c5597c146e45b1481b55f551d2
bvnthb123 <bvnthb123@gmail.com>
2026-09-20 15:42:51 +0700

    Bump version for walkback repair loop

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "2.3.1"
-BUILD_NAME = "Per-Number Walkback Gate"
+VERSION = "2.3.2"
+BUILD_NAME = "Walkback Repair Loop"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"

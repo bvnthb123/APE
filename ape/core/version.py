1685820ed884a746d619cfdacdefc3b2e181da1f
bvnthb123 <bvnthb123@gmail.com>
2026-09-20 21:19:21 +0700

    Bump version for way-top walkback gate

--- ape/core/version.py
+++ ape/core/version.py
@@ -2,6 +2,6 @@
 
 APP_NAME = "Adaptive Prediction Engine"
 APP_CODE = "APE"
-VERSION = "2.3.3"
-BUILD_NAME = "Repair Target Row Fix"
+VERSION = "2.3.4"
+BUILD_NAME = "Walkback Way-Top Gate"
 AUTHOR = "Nguyen Thanh Chung + ChatGPT"
